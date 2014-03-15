02265e1516432f29279066ff1122e64c3893283e
FracturedRetina <admin@krakendev.net>
2014-03-14 20:12:24 -0700

    first commit

---
+++ hack.bat
@@ -0,0 +1,7 @@
+@ECHO off
+COLOR a
+CD \
+
+FOR /L %%I IN (0, 1, 7) DO (
+	TREE
+)
---
+++ no-comic-sans.bat
@@ -0,0 +1,13 @@
+@echo off
+color A
+cd \
+cd Windows\Fonts
+echo Removing Comic Sans TTF...
+erase comic.ttf
+echo.
+echo Removing Comic Sans Bold TTF...
+erase comicbd.ttf
+echo.
+echo.
+echo Your computer has been cleansed.
+pause
