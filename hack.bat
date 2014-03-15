99e212b70a97d042d8ace2f730f26a6f6c9136e5
Evan Shimoniak <eshimoniak@gmail.com>
2014-03-15 11:27:36 -0700

    Commented
    Added /f(list files) to tree for longer tree

--- hack.bat
+++ hack.bat
@@ -1,7 +1,10 @@
 @ECHO off
+::Set console color to green
 COLOR a
+::Set directory to root for longer tree
 CD \
 
 FOR /L %%I IN (0, 1, 7) DO (
-	TREE
+	::Print a hierarchal tree of files and folders
+	TREE /f
 )
